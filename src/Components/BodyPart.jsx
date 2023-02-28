d8af4d39dfe6edfb7b8f6b275dfab6567c3ae46f
Rushikesh Dhanawade <rushikesh26699@gmail.com>
2023-02-28 15:47:04 +0530

    reactjsx-001

---
+++ src/Components/BodyPart.jsx
@@ -0,0 +1,10 @@
+
+function BodyPart(){
+    return <div>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, officia?</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus consequuntur autem modi et quam ratione exercitationem, expedita quisquam commodi vel ipsam recusandae officiis quos? Cumque quasi, rem nesciunt molestias eveniet dolores doloremque obcaecati architecto iure porro? Explicabo quod, sapiente facilis illum aperiam hic aliquam vel dolor necessitatibus tempore facere quos possimus earum. Aliquid earum delectus voluptatum quibusdam alias quod inventore ratione tempore dolorem accusantium ad asperiores ea, necessitatibus quia enim voluptatibus dolorum ab tempora, laborum porro quam quae reiciendis? Deserunt!</p>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus eligendi consequatur veniam itaque nisi aliquid corrupti necessitatibus! Unde, tempora facere.</p>
+    </div>
+}
+
+export default BodyPart;
